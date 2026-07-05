380278d87a8d6ce3ae3a68dcc00b8b3e2e3a3596
mabas <johnmabas1@gmail.com>
2026-07-05 23:14:39 +0100

    add Desposit money function

--- app.js
+++ app.js
@@ -29,8 +29,25 @@ class BankAccount {
     return Math.floor(Math.random() * 1000000000);
   }
 
+  // Deposit money
+  deposit(amount) {
+    if (!this.isActive) {
+      this.message = "Account is inactive.";
+      return;
+    }
+
+    if (amount <= 0) {
+      this.message = "Amount must be greater than 0.";
+      return;
+    }
+
+    this.balance += amount;
+    this.message = `${amount} deposited successfully.`;
+  }
+
 }
 
 const user1 = new BankAccount("Mabas", 54000, "Savings");
+user1.deposit(20000)
 
 console.log(user1);
